930d42186a057a4ee6e4c3e78dd3cadc992d7ddc
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-02 23:18:25 +0200

    Start Updater Function

---
+++ Halmid-Client/Functions/Start_Updater.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halmid_Client.Functions
+{
+    class Start_Updater
+    {
+        public static void Start()
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = AppDomain.CurrentDomain.BaseDirectory + @"Halmid-Updater.exe";
+            Process.Start(start);
+            Environment.Exit(1);
+        }
+    }
+}

1dc0c47cbc51089a7a8752209a22dea1d8abd7af
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-02 21:09:10 +0200

    Encryption/Decryption Function

---
+++ Halmid-Client/Functions/CryptMessage.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography;
+using System.IO;
+
+namespace Halmid_Client.Functions
+{
+    class CryptMessage
+    {
+        public static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted; 
+            using (AesManaged aes = new AesManaged())
+            {   
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);  
+                using (MemoryStream ms = new MemoryStream())
+                {   
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                        {
+                            sw.Write(plainText);
+                        }
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }  
+            return encrypted;
+        }
+
+        public static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;   
+            using (AesManaged aes = new AesManaged())
+            {   
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);  
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {   
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cs))
+                        {
+                            plaintext = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+    }
+}

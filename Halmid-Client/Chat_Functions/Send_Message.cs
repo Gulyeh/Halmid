fdb6bb2b72d4a2b296dd67f04b11f152ac44ee1b
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-02 21:05:54 +0200

    Message encryption

--- Halmid-Client/Chat_Functions/Send_Message.cs
+++ Halmid-Client/Chat_Functions/Send_Message.cs
@@ -1,7 +1,12 @@
 ﻿using Halmid_Client.Connectors;
+using Halmid_Client.Functions;
 using Halmid_Client.Variables;
 using Microsoft.AspNetCore.SignalR.Client;
 using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
 
 namespace Halmid_Client.Chat_Functions
 {
@@ -13,18 +18,34 @@ namespace Halmid_Client.Chat_Functions
             {
                 if (msg != String.Empty)
                 {
-                    await Connector.connection.SendAsync("SendMessage", msg, DateTime.Now.ToString("dd/MM/yyyy - H:mm"), Global_Variables.channelType);
+                    var key = new AesManaged().Key;
+                    var vector = new AesManaged().IV;
+                    var crypted = CryptMessage.Encrypt(msg, key, vector);
+                    var keypair = new Dictionary<string, string>
+                    {
+                        {"key", Convert.ToBase64String(key) + "!" + Convert.ToBase64String(vector)},
+                        {"message", Convert.ToBase64String(crypted)}
+                    };
+                    await Connector.connection.SendAsync("SendMessage", keypair, DateTime.Now.ToString("dd/MM/yyyy - H:mm"), Global_Variables.channelType);
                 }
             }
-            catch (Exception) { }
+            catch (Exception){}
         }
         public static async void Send_ImageMessage(string msg, string imageID)
         {
             try
             {
-                await Connector.connection.SendAsync("Send_ImageMessage", imageID, msg, DateTime.Now.ToString("dd/MM/yyyy - H:mm"), Global_Variables.channelType);
+                var key = new AesManaged().Key;
+                var vector = new AesManaged().IV;
+                var crypted = CryptMessage.Encrypt(msg, key, vector);
+                var keypair = new Dictionary<string, string>
+                    {
+                        {"key", Convert.ToBase64String(key) + "!" + Convert.ToBase64String(vector)},
+                        {"message", Convert.ToBase64String(crypted)}
+                    };
+                await Connector.connection.SendAsync("Send_ImageMessage", imageID, keypair, DateTime.Now.ToString("dd/MM/yyyy - H:mm"), Global_Variables.channelType);
             }
-            catch (Exception) { }
+            catch (Exception){ }
         }
     }
 }

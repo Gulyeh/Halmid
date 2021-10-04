21da1385875d27fb8b408eb8f8fb12b74b1a41f3
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-04 14:38:49 +0200

    Notification Balloon

---
+++ Halmid-Client/Functions/NotifyBallon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Halmid_Client.Functions
+{
+    class NotifyBallon
+    {
+        public static void Show(string text, string title)
+        {
+            var icon = new System.Windows.Forms.NotifyIcon();
+            icon.Icon = new Icon("logo.ico");
+            icon.Visible = true;
+            icon.BalloonTipText = text;
+            icon.BalloonTipTitle = title;
+            icon.ShowBalloonTip(3000);
+        }
+    }
+}

cb5ab0c45c9349abd7475267c53bfc55a5dede14
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-06 01:19:22 +0200

    Error message

--- Halmid-Client/Functions/Start_Updater.cs
+++ Halmid-Client/Functions/Start_Updater.cs
@@ -11,10 +11,16 @@ namespace Halmid_Client.Functions
     {
         public static void Start()
         {
-            ProcessStartInfo start = new ProcessStartInfo();
-            start.FileName = AppDomain.CurrentDomain.BaseDirectory + @"Halmid-Updater.exe";
-            Process.Start(start);
-            Environment.Exit(1);
+            try
+            {
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName = AppDomain.CurrentDomain.BaseDirectory + @"Halmid-Updater.exe";
+                Process.Start(start);
+                Environment.Exit(1);
+            }catch(Exception) 
+            { 
+                MessageBox.Show("Error\nCannot open updater\nPlease check if root folder contains it."); 
+            }
         }
     }
 }

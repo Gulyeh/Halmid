e2046cecb071591d5234c64661a58340284fbd18
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-05 21:16:38 +0200

    Changed to https protocol

--- Halmid-Client/Connectors/Create_HubConnection.cs
+++ Halmid-Client/Connectors/Create_HubConnection.cs
@@ -7,7 +7,7 @@ namespace Halmid_Client.Connectors
         public static void NewConnection()
         {
             Connector.connection = new HubConnectionBuilder()
-                        .WithUrl("http://31.178.21.16:7342/ProtectedChannel").Build();
+                        .WithUrl("https://31.178.21.16:7342/ProtectedChannel").Build();
         }
     }
 }

80efddf40fcc469fbd7aefb3fbf9b64c0a5178cb
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-07 15:32:26 +0200

    Changed version

--- Halmid-Client/Variables/Global_Variables.cs
+++ Halmid-Client/Variables/Global_Variables.cs
@@ -5,7 +5,7 @@ namespace Halmid_Client.Variables
 {
     class Global_Variables
     {
-        public static readonly string Version = "1.27";
+        public static readonly string Version = "1.28";
         public static string Last_userID_onlineMenu { get; set; }
         public static Border last_clickedChannel_Border { get; set; }
         public static string channelType { get; set; }

35e6891ac7e84e696f960f52a5b88c8f7fd8221f
Gulyeh <38474768+Gulyeh@users.noreply.github.com>
2021-10-27 15:04:04 +0200

    changed type of class

--- Halmid-Client/Variables/Global_Variables.cs
+++ Halmid-Client/Variables/Global_Variables.cs
@@ -3,7 +3,7 @@ using System.Windows.Media.Imaging;
 
 namespace Halmid_Client.Variables
 {
-    class Global_Variables
+    static class Global_Variables
     {
         public static readonly string Version = "1.28";
         public static string Last_userID_onlineMenu { get; set; }
